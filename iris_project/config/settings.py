b860251d900ec3f823e6389e71f412df84857471
user-season <hoaxuanhoan@gmail.com>
2026-02-09 12:48:28 +0700

    add csrf_trusted_origin for ngrok

--- iris_project/config/settings.py
+++ iris_project/config/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.ngrok-free.dev',
+    'https://*.ngrok.io',
+]
